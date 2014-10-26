99a05e9e311ce4957ec3f77845f76b027f3a7c99
marcosrimoldi <marcos.rimoldi@gmail.com>
2014-10-26 12:23:33 -0400

    Added fields to choose place, date and time to AddAlarmActivity.

--- app/src/main/java/com/example/rimolma1/buyme/AddAlarmActivity.java
+++ app/src/main/java/com/example/rimolma1/buyme/AddAlarmActivity.java
@@ -1,19 +1,30 @@
 package com.example.rimolma1.buyme;
 
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 
-public class AddAlarmActivity extends ActionBarActivity {
+public class AddAlarmActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
         initActionBar();
+
+        ((RadioButton) findViewById(R.id.radioPlaceType)).performClick();
+
+        initPlaceTypesSpinner();
+
     }
 
 
@@ -36,9 +47,90 @@ public class AddAlarmActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioPlaceType:
+                toggleAddressFields(!checked);
+                toggleTypeFields(checked);
+                break;
+            case R.id.radioPlaceAddress:
+                toggleAddressFields(checked);
+                toggleTypeFields(!checked);
+                break;
+        }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        Bundle args = new Bundle();
+        args.putInt("id", v.getId());
+        newFragment.setArguments(args);
+
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
     private void initActionBar() {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeButtonEnabled(true);
     }
+
+    private void toggleAddressFields(boolean toggle) {
+        if (toggle) {
+            findViewById(R.id.placeAddress).setVisibility(View.VISIBLE);
+            findViewById(R.id.editPlaceAddress).setVisibility(View.VISIBLE);
+        }
+        else {
+            findViewById(R.id.placeAddress).setVisibility(View.INVISIBLE);
+            findViewById(R.id.editPlaceAddress).setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void toggleTypeFields(boolean toggle) {
+        if (toggle) {
+            findViewById(R.id.placeType).setVisibility(View.VISIBLE);
+            findViewById(R.id.placeTypesSpinner).setVisibility(View.VISIBLE);
+        }
+        else {
+            findViewById(R.id.placeType).setVisibility(View.INVISIBLE);
+            findViewById(R.id.placeTypesSpinner).setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void initPlaceTypesSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.placeTypesSpinner);
+        // Defined Array values to show in Spinner
+        String[] values = new String[] { "Supermercado",
+                "Farmacia",
+                "Carniceria",
+                "Verduleria",
+                "Est. de servicio"
+        };
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, values);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
 }
--- app/src/main/java/com/example/rimolma1/buyme/FragmentFirst.java
+++ app/src/main/java/com/example/rimolma1/buyme/FragmentFirst.java
@@ -5,15 +5,73 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by RIMOLMA1 on 10/13/2014.
  */
 public class FragmentFirst extends Fragment {
 
+    private ListView listView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        // Get ListView object from xml
+        listView = (ListView) view.findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(view.getContext().getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+
+        return view;
+
+
     }
 
 }
